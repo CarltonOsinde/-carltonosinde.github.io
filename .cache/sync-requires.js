90f2f4e72d82dcee9b2d6973a01ca9f39b75a932
Carlton Osinde <carltonosinde@gmail.com>
2021-03-02 15:57:44 -0500

    Removed node_module folder

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---node-modules-wkocjan-gatsby-theme-intro-src-pages-404-js": (preferDefault(require("C:\\Users\\Carlton Osinde\\Desktop\\website\\node_modules\\@wkocjan\\gatsby-theme-intro\\src\\pages\\404.js"))),
+  "component---src-pages-404-js": (preferDefault(require("C:\\Users\\Carlton Osinde\\Desktop\\website\\src\\pages\\404.js"))),
+  "component---src-templates-index-js": (preferDefault(require("C:\\Users\\Carlton Osinde\\Desktop\\website\\src\\templates\\index.js")))
+}
+
